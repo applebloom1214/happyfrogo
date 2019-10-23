0c95c6cffc61409f62fb29a16e5342cd52c28307
jje <rainbow610@naver.com>
2019-10-24 00:57:33 +0900

    1.최초 업로드

---
+++ happy/src/main/java/jje/happy/controller/HomeController.java
@@ -0,0 +1,57 @@
+package jje.happy.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
+	}
+	
+	@GetMapping("/happy/index")
+	public String index() {
+		return "/happy/index";
+	}
+	
+	@GetMapping("/happy/sign")
+	public String sign() {
+		return "/happy/sign";
+	}
+	
+	
+	
+}
---
+++ happy/src/main/java/jje/happy/controller/SampleController.java
@@ -0,0 +1,45 @@
+package jje.happy.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("/happy/*")
+@Controller
+public class SampleController {
+
+	@GetMapping("/all")
+	public void doAll() {
+		log.info("기본 화면");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		log.info("로그인 성공, 메인 화면");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		log.info("관리자 페이지");
+	}
+	
+	/*
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')")
+	@GetMapping("/annoMember")
+	public void doMember2() {
+		log.info("logined annotation member");
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		log.info("admin annotation only");
+	}*/
+	
+	
+}
