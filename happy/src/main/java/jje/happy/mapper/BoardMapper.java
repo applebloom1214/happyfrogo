52d0996a3ceca72cdae898cc0403c1ffcb7eb19a
jje <rainbow610@naver.com>
2019-10-25 01:30:06 +0900

    day2 - dd

---
+++ happy/src/main/java/jje/happy/mapper/BoardMapper.java
@@ -0,0 +1,29 @@
+package jje.happy.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import jje.happy.vo.BoardVO;
+//import jje.happy.vo.Criteria;
+
+public interface BoardMapper {
+
+	
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+
+	public Integer insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	/*
+	public List<BoardVO> getListWithPaging(Criteria cri);
+
+	public int getTotalCount(Criteria cri);*/
+}
---
+++ happy/src/main/java/jje/happy/service/BoardService.java
@@ -0,0 +1,24 @@
+package jje.happy.service;
+
+import java.util.List;
+
+import jje.happy.vo.BoardVO;
+
+
+public interface BoardService {
+
+	public void register(BoardVO board);
+
+	public BoardVO get(Long bno);
+
+	public boolean modify(BoardVO board);
+
+	public boolean remove(Long bno);
+
+	 public List<BoardVO> getList();
+
+	//public List<BoardVO> getList(Criteria cri);
+
+	//추가public int getTotal(Criteria cri);
+
+}
